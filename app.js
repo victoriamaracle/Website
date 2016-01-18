dd5afff2cddfa284e38f7d4bdc5e0991fc46f635
victoriamaracle <victoriamaracle@aol.com>
2016-01-18 13:02:06 -0500

    more bug fixes

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 $(document).ready(function() {
-  $('.twitchbutton img').hover(function() {
+  $('.socialcontainer #1').hover(function() {
     $(this).attr('src', '/assets/twitch2.png');
   }, function() {
     $(this).attr('src', '/assets/twitch.png');
   });
 
-   $('.twitterbutton img').hover(function() {
+   $('.socialcontainer #2').hover(function() {
     $(this).attr('src', '/assets/twitter2.png');
   }, function() {
     $(this).attr('src', '/assets/twitter.png');
   });
 
-   $('.facebookbutton img').hover(function() {
+   $('.socialcontainer #3').hover(function() {
     $(this).attr('src', '/assets/facebook2.png');
   }, function() {
     $(this).attr('src', '/assets/facebook.png');
   });
 
-   $('.instagrambutton img').hover(function() {
+   $('.socialcontainer #4').hover(function() {
     $(this).attr('src', '/assets/instagram2.png');
   }, function() {
     $(this).attr('src', '/assets/instagram.png');
   });
 
-   $('.youtubebutton img').hover(function() {
+   $('.socialcontainer #5').hover(function() {
     $(this).attr('src', '/assets/youtube2.png');
   }, function() {
     $(this).attr('src', '/assets/youtube.png');
@@ -33,9 +33,10 @@ $(document).ready(function() {
 $('.twitch-connect').click(function() {
   Twitch.login({
     scope: ['user_read', 'channel_read']
+  });
+})
+
     if (status.authenticated) {
   // Already logged in, hide button
   $('.twitch-connect').hide()
    }
-  });
-})
